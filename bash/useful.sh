c4535e8f128077fddc35605f05c41e71bfd8fd3c
Jamie Tsao <jamie@gametime.co>
2016-12-29 14:22:19 -0800

    More useful commands

--- bash/useful.sh
+++ bash/useful.sh
@@ -3,5 +3,12 @@ for i in 1 2 ; do ssh foo@bar-${i}.com df -h /data ; done
 # deleting line of text from multiple files
 sed -i.bak '/line of text/d' * 
 
+# find and replace recursively in sub-directories
+find . -name "*.txt" -print0 | xargs -0 sed -i '' -e 's/foo/bar/g'
+find ./ -type f -exec sed -i '' -e 's/apple/orange/g' {} \;
+
+# grepping then counting uniq occurrences of a word
+grep timezone * | awk '{print $NF}' | sort | uniq -c
+
 # killing all processees that match the grep pattern
 ps ax | grep pattern | sed 's/ \([0-9]*\).*/\1/' | xargs kill
